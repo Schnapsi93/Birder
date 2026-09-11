b56be6bb3598e70983edc631a07994959b7154ad
Schnapsi93 <aljaz.scetinec@gmail.com>
2026-09-11 21:30:43 +0200

    test_schnapsi93

--- app/src/main/java/com/yourname/birder/QuizActivity.kt
+++ app/src/main/java/com/yourname/birder/QuizActivity.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.R
 
 class QuizActivity : AppCompatActivity() {
 
